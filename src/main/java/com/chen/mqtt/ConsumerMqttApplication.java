ef4b8cc13f23e4ad13b5b96b33e9bf2b561f063d
DS-Chen <tsuchen@outlook.com>
2018-11-14 16:57:37 +0800

    订阅mqtt消息

---
+++ src/main/java/com/chen/mqtt/ConsumerMqttApplication.java
@@ -0,0 +1,15 @@
+package com.chen.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chendongsuo
+ */
+@SpringBootApplication
+public class ConsumerMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMqttApplication.class, args);
+    }
+}
---
+++ src/main/java/com/chen/mqtt/config/MqttReceiverConfig.java
@@ -0,0 +1,88 @@
+package com.chen.mqtt.config;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+
+/**
+ * @author chendongsuo
+ * @create 2018-11-14 13:47
+ * @email dongsuo.chen@nvr-china.com
+ * @description
+ */
+
+@Configuration
+@IntegrationComponentScan
+public class MqttReceiverConfig {
+
+    @Value("${spring.mqtt.username}")
+    private String username;
+
+    @Value("${spring.mqtt.password}")
+    private String password;
+
+    @Value("${spring.mqtt.url}")
+    private String hostUrl;
+
+    @Value("${spring.mqtt.client.id}")
+    private String clientId;
+
+    @Value("${spring.mqtt.default.topic}")
+    private String defaultTopic;
+
+    @Value("${spring.mqtt.completionTimeout}")
+    private Integer completionTimeout;
+
+    @Value("${spring.mqtt.server.id}")
+    private String serverId;
+
+    @Bean
+    public MqttConnectOptions getMqttConnectOptions() {
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setUserName(username);
+        mqttConnectOptions.setPassword(password.toCharArray());
+        mqttConnectOptions.setServerURIs(new String[]{hostUrl});
+        return mqttConnectOptions;
+    }
+
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setConnectionOptions(getMqttConnectOptions());
+        return factory;
+    }
+
+    @Bean
+    public IntegrationFlow mqttInFlow() {
+        return IntegrationFlows.from(mqttInbound())
+                .transform(p -> p + ",receive from mqtt")
+                .handle(logger())
+                .get();
+    }
+
+    private LoggingHandler logger() {
+        LoggingHandler loggingHandler = new LoggingHandler("INFO");
+        loggingHandler.setLoggerName("isSample");
+        return loggingHandler;
+    }
+
+    @Bean
+    public MessageProducerSupport mqttInbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(serverId, mqttClientFactory(), "too");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        return adapter;
+    }
+}
